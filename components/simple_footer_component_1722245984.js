30200c2956d031523c6e808741ee565a455ad3a1
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-29 09:40:33 +0000

    Code generation

--- components/simple_footer_component_1722245984.js
+++ components/simple_footer_component_1722245984.js
@@ -20,7 +20,7 @@ Vue.component("simple_footer_component_1722245984", {
                     </div>
                 </div>
                 <div class="flex justify-center mt-4">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow">
+                    <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105">
                         Join Now
                     </button>
                 </div>
